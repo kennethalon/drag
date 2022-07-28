281ac1b5d5aa4d63cb2487e65e74a316885e8100
JOEL <kennethalon12@gmail.com>
2022-07-28 15:25:12 +0400

    update

--- app/src/main/java/com/example/dragdrop/MainActivity.java
+++ app/src/main/java/com/example/dragdrop/MainActivity.java
@@ -5,103 +5,142 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    ImageView img;
-    String msg;
-    private android.widget.RelativeLayout.LayoutParams layoutParams;
-
-    @SuppressLint("ClickableViewAccessibility")
+public class MainActivity extends AppCompatActivity implements View.OnDragListener, View.OnLongClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img=(ImageView)findViewById(R.id.imageView);
+        //Find all views and set Tag to all draggable views
+        TextView txtVw = (TextView) findViewById(R.id.lbl);
+        txtVw.setTag("DRAGGABLE TEXTVIEW");
+        txtVw.setOnLongClickListener(this);
+        ImageView imgVw = (ImageView) findViewById(R.id.ingvw);
+        imgVw.setTag("ANDROID ICON");
+        imgVw.setOnLongClickListener(this);
+        Button btn = (Button) findViewById(R.id.btnDrag);
+        btn.setTag("DRAGGABLE BUTTON");
+        btn.setOnLongClickListener(this);
+        //Set Drag Event Listeners for defined layouts
+        findViewById(R.id.layout1).setOnDragListener(this);
+        findViewById(R.id.layout2).setOnDragListener(this);
+        findViewById(R.id.layout3).setOnDragListener(this);
+    }
+    @Override
+    public boolean onLongClick(View v) {
+        // Create a new ClipData.Item from the ImageView object's tag
+        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+        // Create a new ClipData using the tag as a label, the plain text MIME type, and
+        // the already-created item. This will create a new ClipDescription object within the
+        // ClipData, and set its MIME type entry to "text/plain"
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+        // Instantiates the drag shadow builder.
+        View.DragShadowBuilder dragshadow = new View.DragShadowBuilder(v);
+        // Starts the drag
+        v.startDrag(data        // data to be dragged
+                , dragshadow   // drag shadow builder
+                , v           // local data about the drag and drop operation
+                , 0          // flags (not currently used, set to 0)
+        );
+        return true;
+    }
+    // This is the method that the system calls when it dispatches a drag event to the listener.
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        // Defines a variable to store the action type for the incoming event
+        int action = event.getAction();
+        // Handles each of the expected events
+        switch (action) {
 
-        img.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
-                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-
-                ClipData dragData = new ClipData(v.getTag().toString(),mimeTypes, item);
-                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(img);
-
-                v.startDrag(dragData,myShadow,null,0);
-                return true;
-            }
-        });
-
-        img.setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                switch(event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        layoutParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
-
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        int x_cord = (int) event.getX();
-                        int y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_EXITED :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        layoutParams.leftMargin = x_cord;
-                        layoutParams.topMargin = y_cord;
-                        v.setLayoutParams(layoutParams);
-                        break;
-
-                    case DragEvent.ACTION_DRAG_LOCATION  :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENDED   :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DROP:
-                        Log.d(msg, "ACTION_DROP event");
-
-                        // Do nothing
-                        break;
-                    default: break;
-                }
-                return true;
-            }
-        });
-
-        img.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    ClipData data = ClipData.newPlainText("", "");
-                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(img);
-
-                    img.startDrag(data, shadowBuilder, img, 0);
-                    img.setVisibility(View.INVISIBLE);
+            case DragEvent.ACTION_DRAG_STARTED:
+                // Determines if this View can accept the dragged data
+                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    // if you want to apply color when drag started to your view you can uncomment below lines
+                    // to give any color tint to the View to indicate that it can accept data.
+                    // v.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
+                    // Invalidate the view to force a redraw in the new tint
+                    //  v.invalidate();
+                    // returns true to indicate that the View can accept the dragged data.
                     return true;
-                } else {
-                    return false;
                 }
-            }
-        });
+                // Returns false. During the current drag and drop operation, this View will
+                // not receive events again until ACTION_DRAG_ENDED is sent.
+                return false;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                // Applies a GRAY or any color tint to the View. Return true; the return value is ignored.
+                v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                // Ignore the event
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                // Re-sets the color tint to blue. Returns true; the return value is ignored.
+                // view.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
+                //It will clear a color filter .
+                v.getBackground().clearColorFilter();
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                // Gets the item containing the dragged data
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                // Gets the text data from the item.
+                String dragData = item.getText().toString();
+                // Displays a message containing the dragged data.
+                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_SHORT).show();
+                // Turns off any color tints
+                v.getBackground().clearColorFilter();
+                // Invalidates the view to force a redraw
+                v.invalidate();
+
+                View vw = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) vw.getParent();
+                owner.removeView(vw); //remove the dragged view
+                //caste the view into LinearLayout as our drag acceptable layout is LinearLayout
+                LinearLayout container = (LinearLayout) v;
+                container.addView(vw);//Add the dragged view
+                vw.setVisibility(View.VISIBLE);//finally set Visibility to VISIBLE
+                // Returns true. DragEvent.getResult() will return true.
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                // Turns off any color tinting
+                v.getBackground().clearColorFilter();
+                // Invalidates the view to force a redraw
+                v.invalidate();
+                // Does a getResult(), and displays what happened.
+                if (event.getResult())
+                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT).show();
+                // returns true; the value is ignored.
+                return true;
+            // An unknown action type was received.
+            default:
+                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
     }
 }
